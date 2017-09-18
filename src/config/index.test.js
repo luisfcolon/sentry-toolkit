65bb4539b84aae7c49f8439bd8124febe93e88bb
Luis Colon <luiscolon62@gmail.com>
2017-09-18 12:33:48 -0400

    Update index.test.js

--- src/config/index.test.js
+++ src/config/index.test.js
@@ -6,5 +6,6 @@ import config from './index';
 describe('Sentry Configration File', () => {
   it('has the expected configuration keys', () => {
     expect(has(config, 'publicDsn')).toEqual(true);
+    expect(has(config, 'environment')).toEqual(true);
   })
 });
